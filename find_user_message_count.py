dc24a18907289d244c7a20d3069da6fa5cf2a74b
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-02-03 02:41:13 +0900

    msg_count

--- find_user_message_count.py
+++ find_user_message_count.py
@@ -1,3 +1,4 @@
+from os import remove
 from read_data import read_data
 from find_all_users_id import find_all_users_id
 
@@ -11,4 +12,24 @@ def find_user_message_count(data: dict, users_id: str)->dict:
     Returns:
         dict: Number of messages of the users
     """
-    return
+    y = data.get("messages")
+    list=[]
+    
+    for i in y:
+        for j in i.values():
+            if j==users_id:
+                a=i.get("type")
+                list.append(a)
+    
+    msg_list=[]
+    for g in list:
+        if g=='message':
+            msg_list.append(g)
+    
+    return len(msg_list)
+
+    
+data=read_data('data/result.json')
+users_idd=find_all_users_id(data)
+users_id=str(users_idd[10])
+print(find_user_message_count(data,users_id))
